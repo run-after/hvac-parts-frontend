62d90184e1ea9c4fc74cb6e5b7a7aa2fb4896ce3
run-after <run.after@protonmail.com>
2021-10-24 16:02:59 -0500

    Add basic layout of single part page with info from DB and ability to delete part

--- src/components/Part.js
+++ src/components/Part.js
@@ -1,10 +1,49 @@
 import '../styles/Part.css';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function Part() {
 
+  const { partID } = useParams();
+
+  const [piece, setPiece] = useState(null);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_DOMAIN}/parts/${partID}`)
+      .then(initRes => {
+        initRes.json().then(part => {
+          setPiece(part);
+        });
+    })
+  }, [partID]);
+
+  const deletePart = () => {
+    // Will need auth to allow this or not
+    fetch(`${process.env.REACT_APP_API_DOMAIN}/parts/${partID}`, {
+      method: 'DELETE'
+    }).then(initRes => {
+      initRes.json().then(part => {
+        window.location = '/parts';
+      });
+    });
+  };
+
   return (
+    piece && 
     <div className='part'>
-      part
+      <button onClick={deletePart}>Delete</button>
+      <p>manufacturer: {piece.manufacturer}</p>
+      <p>partNumber: {piece.partNumber}</p>
+      <p>description: {piece.description}</p>
+      <p>type: {piece.type}</p>
+      <a href={piece.link} target='_blank' rel='noreferrer'>link to buy</a>{/* need to figure out link */}
+      <ul className='unit-list'>
+        {
+          piece.units.map(unit => (
+            <li><a href={`/equipment/${unit._id}`}>{unit.name}</a></li>
+          ))
+        }
+      </ul>
     </div>
   );
   
