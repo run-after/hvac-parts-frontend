4b2e439009db4a0b3a86b45d2c3f11ac8694ece0
run-after <run.after@protonmail.com>
2021-10-24 16:00:32 -0500

    Add basic layout of parts list from DB

--- src/components/Parts.js
+++ src/components/Parts.js
@@ -1,10 +1,30 @@
 import '../styles/Parts.css';
+import { useState, useEffect } from 'react';
 
 function Parts() {
 
+  const [partList, setPartList] = useState(null);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_DOMAIN}/parts`)
+      .then(initRes => {
+        initRes.json().then(part_list => {
+          setPartList(part_list);
+        });
+      });
+  }, []);
+
   return (
     <div className='parts'>
-      parts
+      {
+        partList &&
+        partList.map(piece => (
+          <div key={piece._id} className='part'>
+            <a href={`/parts/${piece._id}`}>{piece.partNumber}</a>
+            <p>{piece.description}</p>            
+          </div>
+        ))
+      }
     </div>
   );
   
